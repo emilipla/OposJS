74077cbb73814bb3b8e700bbda210596f5e2c828
hgsgithub <62034695+hgsgithub@users.noreply.github.com>
2021-03-26 20:18:43 +0100

    25_03

--- Jquery/PreventD.js
+++ Jquery/PreventD.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 
 $('#formulario').submit(function(e){
     e.preventDefault();
-    var nombre = $('#nombre').val()
+    var nombre = $('#nombre').val() // capturo el nombre
+    var apellido = $('#apellido').val()
     console.log(nombre)
+    //alert ("Nombre: "+nombre+" Apellido: "+apellido) se puede utilizar también
+    $('#datos').text(nombre+" "+apellido) // sustituye al getElementById...
+    
 })
 
 
